0e20b376c51cdc5afe7e33ccf9bdeabc3b8409dc
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-10-20 19:38:06 +0200

    Created keyboard component with props text and actionOnClick

---
+++ src/components/Keyboard/Keyboard.js
@@ -0,0 +1,11 @@
+const Keyboard = ({text, actionOnClick}) => {
+  return(
+  <button onClick= {actionOnClick} className="key">{text} </button>;)
+};
+
+Keyboard.propTypes = {
+  text: PropTypes.string.isRequired,
+  actionOnClick: PropTypes.func.isRequired,
+};
+
+export default Keyboard;
